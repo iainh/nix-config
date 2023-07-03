99e1276a7ee80ccc21b474e12eb497d625e1499e
Iain H <iain@spiralpoint.org>
2023-07-03 14:50:55 -0400

    Track unstable rather than a release

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "Your new nix config";
+  description = "IainH nix configuration";
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
