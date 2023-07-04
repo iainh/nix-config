719a21fefa2e290186225ecd0c5994c831f1b06c
Iain H <iain@spiralpoint.org>
2023-07-04 08:20:43 -0400

    Change alacritty theme to Rose Pine, the theme of the day

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -157,41 +157,93 @@
       font.size = 14;
 
       shell.program = "bash";
-      shell.args = ["-l" "-c" "tmux attach || tmux"];
+      # shell.args = ["-l" "-c" "tmux attach || tmux"];
 
-      # Colors (Mellow)
+      # Colors (Rose Pine)
       colors = {
-        # Default colors
         primary = {
-          background = "#161617";
-          foreground = "#c9c7cd";
+          foreground = "#e0def4";
+          background = "#191724";
+          dim_foreground = "#908caa";
+          bright_foreground = "#e0def4";
         };
-        # Cursor colors
-        cursor = {
-          text = "#c9c7cd";
-          cursor = "#757581";
+
+        cursor = {      
+          text = "#e0def4";
+          cursor = "#524f67";
         };
-        # Normal colors
-        normal = {
-          black = "#27272a";
-          red = "#f5a191";
-          green = "#90b99f";
-          yellow = "#e6b99d";
-          blue = "#aca1cf";
-          magenta = "#e29eca";
-          cyan = "#ea83a5";
-          white = "#c1c0d4";
+
+        vi_mode_cursor = {
+          text = "#e0def4";
+          cursor = "#524f67";
         };
-        # Bright colors
+
+        search.matches = {
+          foreground = "#908caa";
+          background = "#26233a";
+        };
+
+        search.focused_match = {
+          foreground = "#191724";
+          background = "#ebbcba";
+        };
+        
+        hints.start = {
+          foreground = "#908caa";
+          background = "#1f1d2e";
+        };
+        
+        hints.end = {
+          foreground = "#6e6a86";
+          background = "#1f1d2e";
+        };
+        
+        line_indicator = {
+          foreground = "None";
+          background = "None";
+        };
+        
+        footer_bar = {
+          foreground = "#e0def4";
+          background = "#1f1d2e";
+        };
+
+        selection = {
+          text = "#e0def4";
+          background = "#403d52";
+        };
+        
+        normal = { 
+          black = "#26233a";
+          red = "#eb6f92";
+          green = "#31748f";
+          yellow = "#f6c177";
+          blue = "#9ccfd8";
+          magenta = "#c4a7e7";
+          cyan = "#ebbcba";
+          white = "#e0def4";
+        };
+
         bright = {
-          black = "#353539";
-          red = "#ffae9f";
-          green = "#9dc6ac";
-          yellow = "#f0c5a9";
-          blue = "#b9aeda";
-          magenta = "#ecaad6";
-          cyan = "#f591b2";
-          white = "#cac9dd";
+          black = "#6e6a86";
+          red = "#eb6f92";
+          green = "#31748f";
+          yellow = "#f6c177";
+          blue = "#9ccfd8";
+          magenta = "#c4a7e7";
+          cyan = "#ebbcba";
+          white = "#e0def4";
+        };
+
+        dim = {
+          black = "#6e6a86";
+          red = "#eb6f92";
+          green = "#31748f";
+          yellow = "#f6c177";
+          blue = "#9ccfd8";
+          magenta = "#c4a7e7";
+          cyan = "#ebbcba";
+          white = "#e0def4";
         };
       };
     };
