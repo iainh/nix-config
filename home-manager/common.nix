bec03b151a0c807935e5eff376b22b4dfa75a2c7
Iain H <iain@spiralpoint.org>
2023-07-29 16:03:34 -0400

    Configure zsh to fit in better with MacOS

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -44,6 +44,42 @@
     EDITOR = "hx";
   };
 
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      hms = "home-manager switch --flake ~/nix-config";
+      grep = "grep --colour=auto";
+      egrep = "grep -E --colour=auto";
+      fgrep = "grep -F --colour=auto";
+      ls = "ls --color=auto";
+    };
+
+    initExtra = ''
+      prompt_gentoo_setup () {
+        local prompt_gentoo_prompt=''${1:-'blue'}
+        local prompt_gentoo_user=''${2:-'green'}
+        local prompt_gentoo_root=''${3:-'red'}
+
+        if [ "$EUID" = '0' ]
+        then
+          local base_prompt="%B%F{$prompt_gentoo_root}%m%k "
+        else
+          local base_prompt="%B%F{$prompt_gentoo_user}%n@%m%k "
+        fi
+        local post_prompt="%b%f%k"
+
+        #setopt noxtrace localoptions
+
+        local path_prompt="%B%F{$prompt_gentoo_prompt}%1~"
+        typeset -g PS1="$base_prompt$path_prompt %# $post_prompt"
+        typeset -g PS2="$base_prompt$path_prompt %_> $post_prompt"
+        typeset -g PS3="$base_prompt$path_prompt ?# $post_prompt"
+      }
+
+      prompt_gentoo_setup "$@"
+    '';
+  };
+
   programs.bash = {
     enable = true;
     shellAliases = {
