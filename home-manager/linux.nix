dc665f9cf2a998ff09f906e9bb6a3f2750e2f908
Iain H <iain@spiralpoint.org>
2024-06-02 11:09:05 -0400

    15 is a better font size for iosevka

--- home-manager/linux.nix
+++ home-manager/linux.nix
@@ -50,7 +50,7 @@ in {
   };
 
   # Reduce font size of alacritty
-  programs.alacritty.settings.font.size = pkgs.lib.mkForce 13;
+  programs.alacritty.settings.font.size = pkgs.lib.mkForce 15;
 
   programs.firefox = {
     enable = true;
